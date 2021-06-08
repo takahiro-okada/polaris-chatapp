69b2d465809b7e1da6ddbe2b2930d4ed91f51109
takahiro okada <t.okada@koshikiad.com>
2021-06-08 09:31:28 +0900

    フォームを追加。フォームは機能しない

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,51 @@
 import React from "react";
-import { EmptyState, Layout, Page } from "@shopify/polaris";
-import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import {useState, useCallback} from "react";
+import { Layout, Page, Card, Button, DataTable, TextField } from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
-class Index extends React.Component {
-	state = { open: false };
-	render() {
-		return (
-			<Page>
-				<TitleBar
-					title="Sample App"
-					primaryAction={{
-						content: "Select products",
-					}}
-				/>
-        <ResourcePicker
-          resourceType="Product"
-          showVariants={false}
-          open={this.state.open}
-          onSelection={(resources) => this.handleSelection(resources)}
-          onCancel={() => this.setState({ open: false })}
-        />
-				<Layout>
-					<EmptyState
-						heading="Discount your products temporarily"
-						action={{
-							content: "Select products",
-              onAction: () => this.setState({ open: true }),
-						}}
-						image={img}
-					>
-						<p>Select products to change their price temporarily.</p>
-					</EmptyState>
-				</Layout>
-			</Page>
-		);
-	}
-  handleSelection = (resources) => {
-    const idsFromResources = resources.selection.map((product) => product.id);
-    this.setState({ open: false });
-    console.log(idsFromResources);
-  };
-}
+export const Index = () => {
+	const [text, setText] = useState('徹子の部屋');
+	const [rooms, setRooms] = useState('');
+  const handleChange = useCallback((newValue) => setValue(newValue), []);
+	const rows = [
+		["A商品", "2021年6月1日",'埋め込みタグ'],
+		["B商品", "2021年6月1日",'埋め込みタグ'],
+		["C商品","2021年6月1日",'埋め込みタグ',],
+	];
+	return (
+		<Page>
+			<TitleBar
+				title="Sample App"
+				primaryAction={{
+					content: "Select products",
+				}}
+			/>
+			<Layout>
+				<Layout.Section>
+					<Card title="ルーム管理" sectioned>
+						<DataTable
+							columnContentTypes={["text", "numeric", "numeric"]}
+							headings={["ルーム名", "部屋の作成日", "アクション"]}
+							rows={rows}
+						/>
+						<form onSubmit={(e) => handleOnSubmit(e)}>
+							<TextField
+							label="ルーム名"
+							value={text}
+							onChange={(e) => setText(e.target.value)}
+							/>
+							<Button
+							primary
+							onSubmit={(e) => handleOnSubmit(e)}
+							>
+							部屋を追加する
+							</Button>
+						</form>
+					</Card>
+				</Layout.Section>
+			</Layout>
+		</Page>
+	);
+};
 export default Index;
